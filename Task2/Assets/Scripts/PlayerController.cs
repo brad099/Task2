dadd7d7e2b616b70a06d29214a4210441aa754a7
B_r_A_d <brad990124@gmail.com>
2022-10-26 01:57:10 +0400

    patch2

--- Task2/Assets/Scripts/PlayerController.cs
+++ Task2/Assets/Scripts/PlayerController.cs
@@ -3,20 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using Dreamteck.Splines;
 using DG.Tweening;
+using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour
 {
-
-
-    SplineFollower _splineFollower;
-    Animator anim;
-    float Horizontal;
-    public float Speed;
-    public float JumpForce;
-    public bool _IsDead=false;
-     Collider[] colliders;
-     [SerializeField]private float DetectionRange;
-     [SerializeField] LayerMask layer;
+    [SerializeField] float Speed;
+    [SerializeField] float JumpForce;
+    [SerializeField]private float DetectionRange;
+    [SerializeField] LayerMask layer;
     [SerializeField]private Transform detectTransform;
     [SerializeField]private Transform holdTransform;
     [SerializeField]private Transform dropTransform;
@@ -24,11 +18,21 @@ public class PlayerController : MonoBehaviour
     [SerializeField]private float ItemDistanceBetween;
     [SerializeField]private float dropCount;
     [SerializeField]private float dropDistanceBetween;
-    Rigidbody rb;
-    float NextDropTime;
-
+    [SerializeField] List<GameObject> CollectedItems;
+    [SerializeField]float DropSecond;
+    [SerializeField]float DropRate;
+    private bool _IsDead=false;
     private bool _isGround= true;
-    // Start is called before the first frame update
+    private float Horizontal;
+    public GameObject RestartLevel;
+    float NextDropTime;
+    Rigidbody rb;
+    Collider[] colliders;
+    SplineFollower _splineFollower;
+    Animator anim;
+    
+    
+    // Getting Components
     void Start()
     {
         rb = GetComponent<Rigidbody>();
@@ -36,106 +40,103 @@ public class PlayerController : MonoBehaviour
         _splineFollower = GetComponentInParent<SplineFollower>();
     }
 
-    // Update is called once per frame
+    // Moving and Jump
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Q) && _isGround)
+        //Moving Jumping
+        if (Input.GetKeyDown(KeyCode.Space) && _isGround)
         {
             anim.SetBool("Jump", true);
             rb.AddForce(Vector3.up * JumpForce,ForceMode.Impulse);
             _isGround=false;
         }
-         if (Input.GetKeyUp(KeyCode.Q))
+         if (Input.GetKeyUp(KeyCode.Space))
         {
             anim.SetBool("Jump", false);
      
         }
         Horizontal = Input.GetAxis("Horizontal");         
-        transform.localPosition += new Vector3(Horizontal, 0, 0) * Time.deltaTime * Speed;
+        transform.localPosition += new Vector3(Horizontal, 0, 0) *Time.deltaTime * Speed;
 
 
 
-
+        //Collecting Tabs
         colliders = Physics.OverlapSphere(detectTransform.position, DetectionRange, layer);
         foreach (var hit in colliders)
         {
             if (hit.CompareTag("Collectable"))
             {
-                Debug.Log(hit.name);
                 hit.tag = "Collected";
                 hit.transform.parent = holdTransform;
 
                  var seq = DOTween.Sequence();
 
                  seq.Append(hit.transform.DOLocalJump(new Vector3(0, itemCount * ItemDistanceBetween), 2, 1, 0.2f))
-                     .Join(hit.transform.DOScale(0.3f, 0.2f));
+                     .Join(hit.transform.DOScale(1f, 0.2f));
                  seq.AppendCallback(() =>
                  {
                      hit.transform.localRotation = Quaternion.Euler(0, 0, 0);
                  });
                  itemCount++;
-
-                // CollectedItems.Add(hit.gameObject);
-
-                /// Type3
-                // Boxes.Push(hit);
-                // var seq = DOTween.Sequence();
-                // seq.Append(hit.transform.DOLocalJump(new Vector3(0, itemCount * ItemDistanceBetween), 2, 1, 0.3f))
-                //    .Join(hit.transform.DOScale(1.25f, 0.1f))
-                //    .Insert(0.1f, hit.transform.DOScale(0.3f, 0.2f));
-                // seq.AppendCallback(() =>
-                // {
-                //     hit.transform.localRotation = Quaternion.Euler(0, 0, 0);
-                // });
-                // itemCount++;
+                 CollectedItems.Add(hit.gameObject);
             }
         }
     }
 
+
+                //Dropping
          public void OnTriggerStay(Collider other)
          {
-
-            other.transform.parent = null;
-            var Seq = DOTween.Sequence();
-            Seq.Append(other.transform.DOJump(dropTransform.position + new Vector3(0,0,(dropCount * dropDistanceBetween )), 2, 1, 0.3f))
-                    .Join(other.transform.DOScale(1.5f, 0.1f))
-                    .Insert(0.1f, other.transform.DOScale(1, 0.2f))
-                    .AppendCallback(() => { other.transform.rotation = Quaternion.Euler(0, 0, 0); });
-            //other.GetComponent<DropArea>().StackedDropItems.Push(go);
-            itemCount--;
+            if (other.transform.CompareTag("Dropzone"))
+        {
+                if (Time.time >= NextDropTime)
+                {
+                    if (CollectedItems.Count > 0)
+                    {
+                     GameObject go = CollectedItems[CollectedItems.Count - 1];
+                     go.transform.parent = null;
+                     var Seq = DOTween.Sequence();
+                     Seq.Append(go.transform.DOJump(dropTransform.position + new Vector3((dropCount * dropDistanceBetween),0, 0), 2, 1, 0.3f))
+                    .Join(go.transform.DOScale(1.2f, 0.1f))
+                    .Insert(0.1f, go.transform.DOScale(1, 0.2f))
+                    .AppendCallback(() => { go.transform.rotation = Quaternion.Euler(0, 0, 0); });
+                     CollectedItems.Remove(go);
+                     dropCount++;
+                     NextDropTime = Time.time + DropSecond / DropRate;
+                     }
+                }
+                anim.SetTrigger("Win");
+                _splineFollower.followSpeed = 0f;
+                Speed = 0f;
+                RestartLevel.SetActive(true);
+        }
          }
 
-
+        // Checking enemy
     public void OnTriggerEnter(Collider other) 
     {
 
         if (other.transform.CompareTag("Enemy"))
         {
-            Debug.Log("we Dead");
             _splineFollower.followSpeed = 0f;
             anim.SetTrigger("Death");
-            _IsDead = true; 
             Speed = 0f;
-        }
-        
-        
+            RestartLevel.SetActive(true);
+        }        
     }
-
+        // Checking ground
      private void OnCollisionEnter(Collision other) 
      {
          if(other.transform.CompareTag("Ground"))
         {
             _isGround=true;
-            Transform child = transform.GetChild(0);
-            child.localRotation = Quaternion.identity;
+            // Transform child = transform.GetChild(0);
+            // child.localRotation = Quaternion.identity;
         }
     }
-
-    public void Death()
+        // Restarting
+    public void Restart()
     {
-        if (_IsDead)
-        {
-            
-        }
+        SceneManager.LoadScene(0);
     }
 }
